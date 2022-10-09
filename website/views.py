ca33b13a196f5b61fe8fcecc3a4a461196aa2ebd
victor <victordj587@gmail.com>
2022-10-09 21:51:10 +0200

    added landing page

--- website/views.py
+++ website/views.py
@@ -1,10 +1,17 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
+import randfacts
 
 views = Blueprint('views', __name__)
 
 
-@views.route('/')
+@views.route('/home')
 @login_required
 def home():
     return render_template("home.html", user=current_user, username=current_user.username)
+
+
+@views.route('/')
+def index():
+    fact = randfacts.getFact()
+    return render_template("index.html", fact=fact)
