9a4923348ebc1238b27e22af8af8f26692469e40
victor <victordj587@gmail.com>
2022-10-09 16:03:32 +0200

    added loging in

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -16,5 +18,18 @@ def create_app():
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+
+    from . import models
+
+    with app.app_context():
+        db.create_all()
+
+    loging_manager = LoginManager()
+    loging_manager.login_view = 'auth.login'
+    loging_manager.init_app(app)
+
+    @loging_manager.user_loader
+    def load_user(id):
+        return models.User.query.get(int(id))
 
     return app
