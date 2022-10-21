6a9b358d5bcd70cb4e0da4b528d42340837cbc57
victor <victordj587@gmail.com>
2022-10-21 22:41:19 +0200

    added progress bar

--- website/static/mySwup.js
+++ website/static/mySwup.js
@@ -1,23 +1,21 @@
-let options = {
-    LINK_SELECTOR: 'a[href^="' + window.location.origin + '"]:not([data-no-swup]), a[href^="/"]:not([data-no-swup]), a[href^="#"]:not([data-no-swup])',
-    FORM_SELECTOR: 'form[data-swup-form]',
-    elements: [
-        '#swup'
-    ],
-    animationSelector: '[class*="transition-"]',
+var swup = new Swup({
     cache: false,
     pageClassPrefix: '',
     scroll: true,
     debugMode: false,
     preload: true,
     support: true,
-    skipPopStateHandling: function(event){
-        if (event.state && event.state.source == "swup") {
-            return false;
-        }
-        return true;
-    },
+    plugins: [
+        new SwupSlideTheme(),
+        new SwupProgressPlugin(
+            {
+                className: 'swup-progress-bar',
+                transition: 300,
+                delay: 0,
+                initialValue: 0.25,
+                hideImmediately: true
+            }
+        ),
+    ],
     animateHistoryBrowsing: true,
-}
-
-const swup = new Swup(options);
+});
